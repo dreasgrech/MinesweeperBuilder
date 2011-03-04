0d04577f9cf229031c2700748d8efaa90d7e3232
Andreas Grech <agnt666@gmail.com>
2011-03-04 01:05:03 +0100

    Oops, actually forgot to commit the MineFieldRow...

---
+++ MinesweeperBuilder/MineFieldRow.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MinesweeperBuilder
+{
+    class MineFieldRow:IEnumerable<char>
+    {
+        private string row;
+        private readonly char mineSymbol;
+
+        public int Length { get { return row.Length; } }
+
+        public int Position { get; private set; }
+        public MineFieldRow(int position, string row, char mineSymbol)
+        {
+            Position = position;
+            this.row = row;
+            this.mineSymbol = mineSymbol;
+        }
+
+        public char GetSymbolAt(int cell)
+        {
+            return row[cell];
+        }
+
+        public bool IsMineAt(int cell)
+        {
+            return GetSymbolAt(cell) == mineSymbol;
+        }
+
+        public IEnumerator<char> GetEnumerator()
+        {
+            return row.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
